17a39f54c8ed9d5c46d8e7c6f37b95289f1f9270
leandrorichard <leandro.richard.go@gmail.com>
2018-06-28 01:52:25 -0300

    Adicionada propriedade hidden para ocultar campos na exibicão dos produtos.

--- app/Produto.php
+++ app/Produto.php
@@ -8,6 +8,15 @@ class Produto extends Model
 {
     protected $table = "produtos";
 
+    protected $hidden = [
+        "peso",
+        "altura",
+        "largura",
+        "profundidade",
+        "created_at",
+        "updated_at",
+    ];
+
     protected $fillable = array(
         "nome",
         "sku",
