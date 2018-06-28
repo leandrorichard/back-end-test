6db57c2b0e2d303098eef4401ebb9d5cb24db065
leandrorichard <leandro.richard.go@gmail.com>
2018-06-28 01:02:16 -0300

    Implementação da classe de teste do crud de produto

--- tests/Feature/CrudProdutoTest.php
+++ tests/Feature/CrudProdutoTest.php
@@ -2,16 +2,81 @@
 
 namespace Tests\Feature;
 
+use App\Produto;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CrudProdutoTest extends TestCase
 {
+    const METHOD_POST   = "POST";
+    const METHOD_PUT    = "PUT";
+    const METHOD_DELETE = "DELETE";
+    const METHOD_GET    = "GET";
+
+    const API_URL = "/api/produto";
+
+    public function testProdutoCreated()
+    {
+        $endpoint = self::API_URL;
+        $data = array(
+            'nome' => 'Console PlayStation 4, Preto',
+            'sku' => 'D279CXX617',
+            'peso' => 3.58,
+            'altura' => 10.2,
+            'largura' => 35.00,
+            'profundidade' => 29.8,
+            'valor' => 2246.60
+        );
+        $headers = array(
+            "Content-Type" => "application/json"
+        );
+
+        $response = $this->json(self::METHOD_POST, $endpoint, $data, $headers);
+        $response->assertStatus(201);
+        $response->assertJson($data);
+    }
+
+    public function testProdutoUpdated()
+    {
+        $endpoint = self::API_URL;
+        $data = array(
+            'nome' => 'Console PlayStation 2, Preto'
+        );
+        $headers = array(
+            "Content-Type" => "application/json"
+        );
+
+        $produto = Produto::create(array(
+            'nome' => 'Console PlayStation 4, Preto',
+            'sku' => 'D279CXX617',
+            'peso' => 3.58,
+            'altura' => 10.2,
+            'largura' => 35.00,
+            'profundidade' => 29.8,
+            'valor' => 2246.60
+        ));
+
+        $response = $this->json(self::METHOD_PUT, "{$endpoint}/{$produto->id}", $data, $headers);
+        $response->assertStatus(200);
+        $response->assertJson($data);
+    }
+
+    public function testProdutoDeleted()
+    {
+        $endpoint = self::API_URL."/1";
+        $data = array();
+        $headers = array(
+            "Content-Type" => "application/json"
+        );
+
+        $response = $this->json(self::METHOD_DELETE, $endpoint, $data, $headers);
+        $response->assertStatus(204);
+    }
+
     public function testProdutoListed()
     {
-        $method = "GET";
-        $endpoint = "/api/produto";
+        $endpoint = self::API_URL;
         $data = array();
         $headers = array(
             "Content-Type" => "application/json"
@@ -31,7 +96,7 @@ class CrudProdutoTest extends TestCase
             )
         );
 
-        $response = $this->json($method, $endpoint, $data, $headers);
+        $response = $this->json(self::METHOD_GET, $endpoint, $data, $headers);
 
         $response->assertStatus(200)
                  ->assertJsonStructure($jsonStructure);
