28a44862cdf9bd5ec4b5e0e02ad64a069c254116
leandrorichard <leandro.richard.go@gmail.com>
2018-06-30 14:11:03 -0300

    Ajuste na classe de teste para novo modelo de roteamento. Adicionado header Accept

--- tests/Feature/CrudProdutoTest.php
+++ tests/Feature/CrudProdutoTest.php
@@ -12,7 +12,7 @@ class CrudProdutoTest extends TestCase
     const METHOD_DELETE = "DELETE";
     const METHOD_GET    = "GET";
 
-    const API_URL = "/api/produto";
+    const API_URL = "/api/v1/produto";
 
     public function testProdutoCreated()
     {
@@ -34,7 +34,9 @@ class CrudProdutoTest extends TestCase
             )
         );
         $headers = array(
-            "Content-Type" => "application/json"
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
+
         );
 
         $response = $this->json(self::METHOD_POST, $endpoint, $data, $headers);
@@ -45,13 +47,20 @@ class CrudProdutoTest extends TestCase
     public function testProdutoUpdated()
     {
         $endpoint = self::API_URL;
-        $data = array(
-            "nome" => "Console PlayStation 2, Preto"
-        );
         $headers = array(
-            "Content-Type" => "application/json"
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
         );
 
+        $dataUpdate = array(
+            "nome" => "Console PlayStation 2, Branco",
+            "sku" => "D279CXX617",
+            "peso" => 3.58,
+            "altura" => 10.2,
+            "largura" => 35.00,
+            "profundidade" => 29.8,
+            "valor" => 2326.60
+        );
         $produto = Produto::create(array(
             "nome" => "Console PlayStation 4, Preto",
             "sku" => "D279CXX617",
@@ -65,11 +74,11 @@ class CrudProdutoTest extends TestCase
                 "id" => $produto->id,
                 "nome" => "Console PlayStation 2, Preto",
                 "sku" => "D279CXX617",
-                "valor" => 2246.60
+                "valor" => 2326.60
             )
         );
 
-        $response = $this->json(self::METHOD_PUT, "{$endpoint}/{$produto->id}", $data, $headers);
+        $response = $this->json(self::METHOD_PUT, "{$endpoint}/{$produto->id}", $dataUpdate, $headers);
         $response->assertStatus(200);
         $response->assertJson($dataAssert);
     }
@@ -79,7 +88,8 @@ class CrudProdutoTest extends TestCase
         $endpoint = self::API_URL."/1";
         $data = array();
         $headers = array(
-            "Content-Type" => "application/json"
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
         );
 
         $response = $this->json(self::METHOD_DELETE, $endpoint, $data, $headers);
@@ -91,7 +101,8 @@ class CrudProdutoTest extends TestCase
         $endpoint = self::API_URL;
         $data = array();
         $headers = array(
-            "Content-Type" => "application/json"
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
         );
         $jsonStructure = array("data" =>
             array("*" =>
