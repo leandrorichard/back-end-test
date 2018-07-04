f25a8e7d83e54dcdb15813bc1dd98b7fd467f6f4
leandrorichard <leandro.richard.go@gmail.com>
2018-07-04 19:25:20 -0300

    Adicionada classe de teste do calculo de frete

---
+++ tests/Feature/CalculaFreteTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class CalculaFreteTest extends TestCase
+{
+    const METHOD_POST   = "POST";
+    const METHOD_PUT    = "PUT";
+    const METHOD_DELETE = "DELETE";
+    const METHOD_GET    = "GET";
+
+    const API_URL = "/api/v1/calculos/correios?pid=1&cep_origem=74911350&cep_destino=70690000";
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('db:seed');
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testJson()
+    {
+        $endpoint = self::API_URL;
+        $data = array();
+        $headers = array(
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
+        );
+        $jsonStructure = array(
+            array(
+                "valor",
+                "prazo"
+            )
+        );
+
+        $response = $this->json(self::METHOD_GET, $endpoint, $data, $headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure($jsonStructure);
+    }
+}
