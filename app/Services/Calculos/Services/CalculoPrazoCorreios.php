f00fc15be6d1d295f20861161d02c6561f46e88d
leandrorichard <leandro.richard.go@gmail.com>
2018-07-04 17:50:17 -0300

    Adicionado repositorio para obter o produto para calculo do frete.

--- app/Services/Calculos/Services/CalculoPrazoCorreios.php
+++ app/Services/Calculos/Services/CalculoPrazoCorreios.php
@@ -3,40 +3,62 @@
 namespace App\Services\Calculos\Services;
 
 use App\Http\Requests\CalculoPrecoRequest;
+use App\Repositories\Produto\RepositoryContract;
 use App\Services\Calculos\Contracts\CalculoPrazoContract;
-use Symfony\Component\HttpFoundation\Response;
 
 class CalculoPrazoCorreios implements CalculoPrazoContract
 {
     protected $url;
+    protected $produtoRepository;
 
-    public function __construct()
+    public function __construct(RepositoryContract $repositoryContract)
     {
         $this->url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx';
+        $this->produtoRepository = $repositoryContract;
     }
 
 
-    public function calcular(CalculoPrecoRequest $request): Response
+    public function calcular(CalculoPrecoRequest $request): array
     {
-        $correio = new Correios();
-        $correio->setNCdEmpresa('');
-        $correio->setSDsSenha('');
-        $correio->setSCepOrigem('96010140');
-        $correio->setSCepDestino('02460000');
-        $correio->setNVlPeso('1');
-        $correio->setNCdFormato('1');
-        $correio->setNVlComprimento('16');
-        $correio->setNVlAltura('5');
-        $correio->setNVlLargura('15');
-        $correio->setNVlDiametro('0');
-        $correio->setSCdMaoPropria('s');
-        $correio->setNVlValorDeclarado('200');
-        $correio->setSCdAvisoRecebimento('n');
-        $correio->setStrRetorno('xml');
-        $correio->setNCdServico('40010,41106');
+        try {
+            $produto = $this->produtoRepository->find($request->input('pid'));
+            $cepOrigem = $request->input('cep_origem');
+            $cepDestino = $request->input('cep_destino');
 
-        $dados = Client::send($correio->toArray(), $this->url);
+            $correio = new Correios();
+            $correio->setNCdEmpresa('');
+            $correio->setSDsSenha('');
+            $correio->setSCepOrigem($cepOrigem);
+            $correio->setSCepDestino($cepDestino);
+            $correio->setNVlPeso($produto->peso);
+            $correio->setNCdFormato('1');
+            $correio->setNVlComprimento($produto->profundidade);
+            $correio->setNVlAltura($produto->altura);
+            $correio->setNVlLargura($produto->largura);
+            $correio->setNVlDiametro(16);
+            $correio->setSCdMaoPropria('s');
+            $correio->setNVlValorDeclarado($produto->valor);
+            $correio->setSCdAvisoRecebimento('n');
+            $correio->setStrRetorno('xml');
+            $correio->setNCdServico('40010');
 
-        dd($dados->cServico);
+            $retorno = Client::send($correio->toArray(), $this->url);
+            return $this->processaRetorno($retorno);
+        } catch (\Exception $e) {
+            throw new $e;
+        }
+
+    }
+
+    public function processaRetorno(\SimpleXMLElement $xml): array
+    {
+        $servico = $xml->cServico;
+        $valor = (string) $servico->Valor[0];
+        $prazo = (string) $servico->PrazoEntrega[0];
+
+        return array(
+            "valor" => $valor,
+            "prazo" => $prazo
+        );
     }
 }
