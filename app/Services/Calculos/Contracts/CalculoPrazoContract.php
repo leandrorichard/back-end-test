7bb4611ffd6f429e50e9a681c7a7cc315fbe2694
leandrorichard <leandro.richard.go@gmail.com>
2018-07-04 17:49:44 -0300

    Adicionado metodo para processar retorno. Alterado o tipo de retorno dos metodos.

--- app/Services/Calculos/Contracts/CalculoPrazoContract.php
+++ app/Services/Calculos/Contracts/CalculoPrazoContract.php
@@ -3,9 +3,10 @@
 namespace App\Services\Calculos\Contracts;
 
 use App\Http\Requests\CalculoPrecoRequest;
-use Symfony\Component\HttpFoundation\Response;
 
 interface CalculoPrazoContract
 {
-    public function calcular(CalculoPrecoRequest $request): Response;
+    public function calcular(CalculoPrecoRequest $request): array;
+
+    public function processaRetorno(\SimpleXMLElement $xml): array;
 }
